136916c8e3c425018f785d77df1d4a983fd7f4d1
Miguel González Ortiz <miggonort1@alum.us.es>
2024-05-26 12:26:26 +0200

    Dashboard fixed

--- src/main/java/acme/features/sponsor/dashboard/SponsorDashboardShowService.java
+++ src/main/java/acme/features/sponsor/dashboard/SponsorDashboardShowService.java
@@ -64,12 +64,28 @@ public class SponsorDashboardShowService extends AbstractService<Sponsor, Sponso
 		deviationSponsorshipsAmount.setAmount(this.sdr.deviationSponsorshipsAmount(id));
 		deviationSponsorshipsAmount.setCurrency("EUR");
 
+		Money averageSponsorshipsAmount0 = new Money();
+		averageSponsorshipsAmount.setAmount(00.00);
+		averageSponsorshipsAmount.setCurrency("EUR");
+
+		Money deviationSponsorshipsAmount0 = new Money();
+		deviationSponsorshipsAmount.setAmount(00.00);
+		deviationSponsorshipsAmount.setCurrency("EUR");
+
 		Money minimumSponsorshipsAmount = new Money();
 		minimumSponsorshipsAmount.setAmount(this.sdr.minimumSponsorshipsAmount(id));
 		minimumSponsorshipsAmount.setCurrency("EUR");
 
+		Money minimumSponsorshipsAmount0 = new Money();
+		minimumSponsorshipsAmount.setAmount(00.00);
+		minimumSponsorshipsAmount.setCurrency("EUR");
+
 		Money maximumSponsorshipsAmount = new Money();
 		maximumSponsorshipsAmount.setAmount(this.sdr.maximumSponsorshipsAmount(id));
+		maximumSponsorshipsAmount.setCurrency("EUR");
+
+		Money maximumSponsorshipsAmount0 = new Money();
+		maximumSponsorshipsAmount.setAmount(00.00);
 		maximumSponsorshipsAmount.setCurrency("EUR");
 
 		Money averageInvoicesQuantity = new Money();
@@ -80,6 +96,14 @@ public class SponsorDashboardShowService extends AbstractService<Sponsor, Sponso
 		deviationInvoicesQuantity.setAmount(this.sdr.deviationInvoicesQuantity(id));
 		deviationInvoicesQuantity.setCurrency("EUR");
 
+		Money averageInvoicesQuantity0 = new Money();
+		averageInvoicesQuantity.setAmount(00.00);
+		averageInvoicesQuantity.setCurrency("EUR");
+
+		Money deviationInvoicesQuantity0 = new Money();
+		deviationInvoicesQuantity.setAmount(00.00);
+		deviationInvoicesQuantity.setCurrency("EUR");
+
 		Money minimumInvoicesQuantity = new Money();
 		minimumInvoicesQuantity.setAmount(this.sdr.minimumInvoicesQuantity(id));
 		minimumInvoicesQuantity.setCurrency("EUR");
@@ -88,21 +112,41 @@ public class SponsorDashboardShowService extends AbstractService<Sponsor, Sponso
 		maximumInvoicesQuantity.setAmount(this.sdr.maximumInvoicesQuantity(id));
 		maximumInvoicesQuantity.setCurrency("EUR");
 
+		Money minimumInvoicesQuantity0 = new Money();
+		minimumInvoicesQuantity.setAmount(00.00);
+		minimumInvoicesQuantity.setCurrency("EUR");
+
+		Money maximumInvoicesQuantity0 = new Money();
+		maximumInvoicesQuantity.setAmount(00.00);
+		maximumInvoicesQuantity.setCurrency("EUR");
+
 		totalNumberOfInvoicesWithTaxLessOrEquals21 = this.sdr.totalNumberOfInvoicesWithTaxLessOrEquals21(id);
 		totalNumberOfSponsorshipsLink = this.sdr.totalNumberOfSponsorshipsLink(id);
 
 		dashboard = new SponsorDashboard();
-		dashboard.setTotalNumberOfInvoicesWithTaxLessOrEquals21(totalNumberOfInvoicesWithTaxLessOrEquals21);
-		dashboard.setTotalNumberOfSponsorshipsLink(totalNumberOfSponsorshipsLink);
-		dashboard.setAverageSponsorshipsAmount(averageSponsorshipsAmount);
-		dashboard.setDeviationSponsorshipsAmount(deviationSponsorshipsAmount);
-		dashboard.setMinimumSponsorshipsAmount(minimumSponsorshipsAmount);
-		dashboard.setMaximumSponsorshipsAmount(maximumSponsorshipsAmount);
-		dashboard.setAverageInvoicesQuantity(averageInvoicesQuantity);
-		dashboard.setDeviationInvoicesQuantity(deviationInvoicesQuantity);
-		dashboard.setMinimumInvoicesQuantity(minimumInvoicesQuantity);
-		dashboard.setMaximumInvoicesQuantity(maximumInvoicesQuantity);
+		dashboard.setTotalNumberOfInvoicesWithTaxLessOrEquals21(0);
+		dashboard.setTotalNumberOfSponsorshipsLink(0);
+		dashboard.setMinimumSponsorshipsAmount(minimumSponsorshipsAmount0);
+		dashboard.setMaximumSponsorshipsAmount(maximumSponsorshipsAmount0);
+		dashboard.setMinimumInvoicesQuantity(minimumInvoicesQuantity0);
+		dashboard.setMaximumInvoicesQuantity(maximumInvoicesQuantity0);
+		dashboard.setAverageSponsorshipsAmount(averageSponsorshipsAmount0);
+		dashboard.setDeviationSponsorshipsAmount(deviationSponsorshipsAmount0);
+		dashboard.setAverageInvoicesQuantity(averageInvoicesQuantity0);
+		dashboard.setDeviationInvoicesQuantity(deviationInvoicesQuantity0);
 
+		if (averageSponsorshipsAmount != null) {
+			dashboard.setTotalNumberOfInvoicesWithTaxLessOrEquals21(totalNumberOfInvoicesWithTaxLessOrEquals21);
+			dashboard.setTotalNumberOfSponsorshipsLink(totalNumberOfSponsorshipsLink);
+			dashboard.setAverageSponsorshipsAmount(averageSponsorshipsAmount);
+			dashboard.setDeviationSponsorshipsAmount(deviationSponsorshipsAmount);
+			dashboard.setMinimumSponsorshipsAmount(minimumSponsorshipsAmount);
+			dashboard.setMaximumSponsorshipsAmount(maximumSponsorshipsAmount);
+			dashboard.setAverageInvoicesQuantity(averageInvoicesQuantity);
+			dashboard.setDeviationInvoicesQuantity(deviationInvoicesQuantity);
+			dashboard.setMinimumInvoicesQuantity(minimumInvoicesQuantity);
+			dashboard.setMaximumInvoicesQuantity(maximumInvoicesQuantity);
+		}
 		super.getBuffer().addData(dashboard);
 	}
 
