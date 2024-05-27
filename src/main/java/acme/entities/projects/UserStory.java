e271af4c73588d18b564d311f857e40829f06bd2
JesusFern <jfr16se>
2024-05-27 11:59:35 +0200

    Indexes added

--- src/main/java/acme/entities/projects/UserStory.java
+++ src/main/java/acme/entities/projects/UserStory.java
@@ -2,7 +2,9 @@
 package acme.entities.projects;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -20,6 +22,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "id"), @Index(columnList = "manager_id")
+})
 public class UserStory extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
