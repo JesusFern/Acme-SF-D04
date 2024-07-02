99add69f800b7d614204570bc944a553c00c8db0
luiperfra1 <luiperfra1@alum.us.es>
2024-07-02 12:22:38 +0200

    Conversor horasEuros

--- src/main/java/acme/features/client/contract/ClientContractPublishService.java
+++ src/main/java/acme/features/client/contract/ClientContractPublishService.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.client.data.datatypes.Money;
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
 import acme.client.views.SelectChoices;
@@ -70,21 +71,56 @@ public class ClientContractPublishService extends AbstractService<Client, Contra
 			existing = this.ccr.findOneContractByCode(object.getCode());
 			super.state(existing == null || existing.equals(object), "code", "client.contract.form.error.duplicated");
 		}
-
+		if (!super.getBuffer().getErrors().hasErrors("budget")) {
+			super.state(object.getBudget().getAmount() >= 0, "budget", "client.contract.form.error.negative-budget");
+			super.state(object.getBudget().getAmount() <= 1000000, "budget", "client.contract.form.error.much-budget");
+			String currency = object.getBudget().getCurrency();
+			boolean isValidCurrency = currency.equals("EUR") || currency.equals("GBP") || currency.equals("USD");
+			super.state(isValidCurrency, "budget", "client.contract.form.error.invalid-currency");
+		}
 		{
 			double totalBudgets = 0.0;
 			Project project = object.getProject();
-
+			Money budget = new Money();
 			Collection<Contract> contracts = this.ccr.findManyContractsAvailableByProjectId(project.getId());
-			for (Contract c : contracts)
-				totalBudgets += c.getBudget().getAmount();
-			totalBudgets += object.getBudget().getAmount();
+			for (Contract c : contracts) {
+				budget = this.ConvertToEuros(c.getBudget());
+				totalBudgets += budget.getAmount();
+			}
+
+			totalBudgets += this.ConvertToEuros(object.getBudget()).getAmount();
 			double projectCost = object.getProject().getCost();
-			super.state(totalBudgets <= projectCost, "*", "client.contract.form.error.exceeded-project-cost");
+			Money projectCostEUR = this.hoursToEuros(projectCost);
+
+			super.state(totalBudgets <= projectCostEUR.getAmount(), "*", "client.contract.form.error.exceeded-project-cost");
 
 		}
 	}
+	public Money hoursToEuros(double projectCost) {
+		projectCost = projectCost * 150;
+		Money projectCostEUR = new Money();
+		projectCostEUR.setAmount(projectCost);
+		projectCostEUR.setCurrency("EUR");
+		return projectCostEUR;
+	}
 
+	public Money ConvertToEuros(final Money dinero) {
+		if (dinero.getCurrency().equals("GBP")) {
+			dinero.setAmount(dinero.getAmount() * 1.18);
+			dinero.setCurrency("EUR");
+			return dinero;
+		}
+
+		else if (dinero.getCurrency().equals("USD")) {
+			dinero.setAmount(dinero.getAmount() * 0.94);
+			dinero.setCurrency("EUR");
+			return dinero;
+		}
+
+		else
+			return dinero;
+
+	}
 	@Override
 	public void perform(final Contract object) {
 		assert object != null;
